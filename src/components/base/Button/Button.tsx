7efb1d81a4a77bbd4ebeb8a1c091c1b621ed313d
suyeon-hong <78653426+suyeon-hong@users.noreply.github.com>
2022-02-07 21:26:32 +0900

    :sparkles:  Feat: 버튼 구현 (#6)
    Closes #1

---
+++ src/components/base/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from "react";
+import * as S from "./Style";
+
+interface Props {
+	children: string;
+	blue: boolean;
+}
+
+const Button = ({ children, blue }: Props): ReactElement => {
+	return (
+		<>
+			<S.Button blue={blue}>{children}</S.Button>
+		</>
+	);
+};
+
+export default Button;
