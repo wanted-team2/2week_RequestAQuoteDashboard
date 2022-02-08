475b1cdc3f94ca7c35ba2eabab15937eb31d6236
sueyon-hong <bellahong95@gmail.com>
2022-02-08 14:46:37 +0900

    :recycle:  Refactoring: ForwardProps Partial타입으로 수정

--- src/components/base/Toggle/Toggle.tsx
+++ src/components/base/Toggle/Toggle.tsx
@@ -9,16 +9,23 @@ import useToggle from '@hooks/useToggle';
 import * as S from './Style';
 
 interface ForwardProps {
-  isToggle?: false;
-  disabled?: false;
-  onChange?(e: ChangeEvent<HTMLInputElement>): void;
-  children?: ReactChild | ReactChildren;
-  name?: string;
+  isToggle: false;
+  disabled: false;
+  onChange(e: ChangeEvent<HTMLInputElement>): void;
+  children: ReactChild | ReactChildren;
+  name: string;
 }
 
 const Toggle = forwardRef(
   (
-    { isToggle, disabled, onChange, name, children, ...props }: ForwardProps,
+    {
+      isToggle,
+      disabled,
+      onChange,
+      name,
+      children,
+      ...props
+    }: Partial<ForwardProps>,
     ref: ForwardedRef<HTMLInputElement>
   ) => {
     const [checked, onToggle] = useToggle({ initialState: isToggle });
