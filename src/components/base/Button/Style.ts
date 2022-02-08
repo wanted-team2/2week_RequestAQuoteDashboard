05f71ceb3bc526c1fbbecdab58c86bfbc792edf6
sueyon-hong <bellahong95@gmail.com>
2022-02-08 10:44:51 +0900

    :sparkles:  Feat: EmptyBox 컴포넌트 구현
    Closes #12

--- src/components/base/Button/Style.ts
+++ src/components/base/Button/Style.ts
@@ -1,17 +1,17 @@
-import styled from "styled-components";
-import { font14 } from "@styles/fonts";
+import styled from 'styled-components';
+import { font14 } from '@styles/fonts';
 
 interface IBtn {
-	blue: boolean;
+  blue: boolean;
 }
 
 export const Button = styled.button<IBtn>`
-	${font14(500)};
-	color: ${({ blue }) => (blue ? "#fff" : ({ theme }) => theme.colors.skyBlue)};
-	border: 1px solid ${({ theme }) => theme.colors.skyBlue};
-	border-radius: ${({ theme }) => `${theme.size.borderRadius}px`};
-	background: ${({ blue }) =>
-		blue ? ({ theme }) => theme.colors.skyBlue : "#fff"};
-	padding: ${({ theme }) => `6px ${theme.gap.base * 3}px`};
-	margin-right: ${({ theme }) => `${theme.gap.base * 3}px`};
+  ${font14(500)};
+  color: ${({ blue }) => (blue ? '#fff' : ({ theme }) => theme.colors.skyBlue)};
+  border: 1px solid ${({ theme }) => theme.colors.skyBlue};
+  border-radius: ${({ theme }) => `${theme.size.borderRadius}px`};
+  background: ${({ blue }) =>
+    blue ? ({ theme }) => theme.colors.skyBlue : '#fff'};
+  padding: ${({ theme }) => `6px ${theme.gap.base * 3}px`};
+  margin-right: ${({ theme }) => `${theme.gap.base * 3}px`};
 `;
--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button/Button';
 export { default as Buttons } from './Buttons/Buttons';
 export { default as Toggle } from './Toggle/Toggle';
+export { default as EmptyBox } from './EmptyBox/EmptyBox';
